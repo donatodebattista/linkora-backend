5f78c9779d089465ca1c553de3ef71582f69b264
donatodebattista <doni.db@hotmail.com>
2025-02-18 21:10:22 -0300

    feat: add User model

---
+++ src/models/User.ts
@@ -0,0 +1,30 @@
+import mongoose, {Schema} from "mongoose";
+
+interface IUser {
+    name: String
+    email: String
+    password: String
+}
+
+const userSchema = new Schema({
+    name:{
+        type: String,
+        required: true,
+        trim: true
+    },
+    email:{
+        type: String,
+        required: true,
+        trim: true,
+        unique: true
+    },
+    password:{
+        type: String,
+        required: true,
+        trim: true
+    }
+})
+
+
+const User = mongoose.model<IUser>('User', userSchema)
+export default User
