12f2183340e14f630f66f4b0c07ed1201023f1d4
Donato De Battista <doni.db@hotmail.com>
2025-05-12 19:08:54 -0300

    fix: slug handle

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,22 +1,19 @@
 import type { Request, Response } from 'express'
 import { validationResult } from 'express-validator'
-import slug from 'slug'
+import slugify from 'slugify'
 import User from '../models/User'
 import { hashPassword } from '../utils/auth'
 
 export const createAccount = async (req: Request, res: Response) => {
-
-
         //Manejo de error validacion de datos
         let errors = validationResult(req)
         if(! errors.isEmpty()){
             res.status(400).json({errors: errors.array()})
             return
         }
-
-
         
-        const {email, password} = req.body
+        //Manejo de error si el usuario ya existe
+        const {email, password, handle} = req.body
         const userExist = await User.findOne({email})
         if (userExist){
             const error = new Error('El correo ingresado ya se encuentra registrado')
@@ -24,19 +21,24 @@ export const createAccount = async (req: Request, res: Response) => {
             return  
         } 
         
-        const handle = slug(req.body.handle, '')
-
-        const handleExist = await User.findOne({handle})
+        //Manejo de error si el nombre de usuario ya existe
+        const handleSlug = slugify(handle)
+        const handleExist = await User.findOne({handle: handleSlug})
+        console.log(handleExist)
         if (handleExist){
             const error = new Error('El nombre de usuario no esta disponible')
             res.status(409).json({error : error.message})
             return
         } 
-            
+        
+        console.log('Sigue la ejecucion 1')
+
         const user = new User(req.body)
         user.password = await hashPassword(password)
-        user.handle = handle
+        user.handle = handleSlug
         
+        console.log('Sigue la ejecucion 2')
+
         await user.save()
         res.status(201).send('Usuario creado correctamente')
 
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import colors from 'colors'
 import server from "./server"
 
-const port = process.env.port || 4000
+const port = process.env.port || 3000
 
 server.listen(port, () => {
   console.log(colors.bgCyan.black(`Running on port ${port}🚀`))
