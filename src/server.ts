944828a946afa40a34f08ad5c49dbb8fa22bf48c
Donato De Battista <doni.db@hotmail.com>
2025-06-26 00:12:26 -0300

    link to front

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,15 @@
 import express from 'express'
 import 'dotenv/config'
 import router from './router'
+import cors from 'cors'
+import { corsConfig } from './config/cors'
 import { connectDB } from './config/db'
 
 const app = express()
 connectDB()
+
+//CORS
+app.use(cors(corsConfig))
 
 //Habilitar lectura de datos de form
 app.use(express.json())
